51f4ec01e42c0513a2c08af50905f64f4924e060
César Salgueiro <csalgueiro.dev@gmail.com>
2021-06-02 20:07:57 +0200

    Adds consts to width and height of cards changes eventlisteners to document converts the animation into a closure to avoid execution if clicked severaltimes Changes styles of card and board

--- script.js
+++ script.js
@@ -1,28 +1,43 @@
 const numbers = ["2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K", "A"];
 const suits = ["♠", "♥", "♣", "♦"];
-const cardRemOffset = [6, 10]
+const offset = { width: 6, height: 10 }
 const spacing = 0.5;
+const cardsByLine = 12;
 const units = "rem";
+let stopAnimation = false;
 
 const board = document.querySelector("#board")
 const shuffleBtn = document.querySelector(".btn[data-action='shuffle']")
 const dealBtn = document.querySelector(".btn[data-action='deal']")
 const resetBtn = document.querySelector(".btn[data-action='reset']")
 
-
 createAllCards()
 cards = [...document.querySelectorAll(".card")]
 reset(cards);
 
-shuffleBtn.addEventListener("click", () => {
-    shuffle(cards)
-})
-dealBtn.addEventListener("click", () => {
-    deal(cards)
-})
-resetBtn.addEventListener("click", () => {
-    cards = [...document.querySelectorAll(".card")]
-    reset(cards)
+document.addEventListener("click", (e) => {
+    let target = e.target;
+    switch (target.nodeName) {
+        case "BUTTON":
+            if (target.classList.contains("btn")) {
+                if (target.dataset.action == "deal") {
+                    deal(cards)
+                }
+                if (target.dataset.action == "shuffle") {
+                    shuffle(cards)
+                }
+                if (target.dataset.action == "reset") {
+                    cards = [...document.querySelectorAll(".card")]
+                    reset(cards)
+                }
+            }
+            break;
+        case "DIV":
+            if (target.classList.contains("topCard")) {
+                deal(cards)
+            }
+            break;
+    }
 })
 
 function createAllCards() {
@@ -43,33 +58,40 @@ function createCard(number, suit) {
 }
 
 function deal(cards) {
+    var execId = deal.execId = (deal.execId || 0) + 1;
     let line = 0;
     let left = 0;
     let top = 0;
     let cardsDealed = 0;
-    cards.forEach((card, idx) => {
-        let i = idx;
-        card.style.transform = "translate(0" + units + ",0" + units + ")"
-        setTimeout(() => {
-            card.style.zIndex = cardsDealed
-            if (cardsDealed % 12 == 0) {
-                line++;
-                left = cardRemOffset[0] + spacing
-                top += cardRemOffset[1] + spacing
-            } else {
-                left += cardRemOffset[0] + spacing
-            }
-            card.style.transform = "translate(" + left + units + "," + top + units + ")"
-            cardsDealed++
-
-        }, idx * 50)
-    });
+    for (let i = 0; i < cards.length; i++) {
+        (function (card, idx) {
+            card.classList.remove("topCard")
+            card.style.transform = "translate(0" + units + ",0" + units + ")"
+            setTimeout(function () {
+                if (deal.execId != execId) {
+                    return false;
+                }
+                card.style.zIndex = cardsDealed
+                if (cardsDealed % cardsByLine == 0) {
+                    line++;
+                    left = spacing
+                    top += offset.height + spacing
+                } else {
+                    left += offset.width + spacing
+                }
+                card.style.transform = "translate(" + left + units + "," + top + units + ")"
+                cardsDealed++
+            }, idx * 50)
+        })(cards[i], i);
+    }
 }
 
 function reset(cards) {
+    const len = cards.length
+    cards[0].classList.add("topCard")
     cards.forEach((card, i) => {
         card.style.transform = "translate(0." + i + units + ",0." + i + units + ")"
-        card.style.zIndex = cards.length - i
+        card.style.zIndex = len - i
     })
 }
 
